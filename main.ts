97215faf3de6de6e2169c7f1f9234127496fe4ec
Limbo8rr <77548425+Limbo8rr@users.noreply.github.com>
2021-09-11 23:12:47 -0700

    i added some code and made it into a simple game

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Weapon = SpriteKind.create()
+}
 function useWeapon () {
     if (!(boolWeaponOut)) {
         AButtonLastPressedTime = game.runtime()
@@ -26,8 +29,116 @@ function useWeapon () {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     useWeapon()
 })
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Weapon, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprite.destroy()
+    spawnEnemy()
+})
+function spawnEnemy () {
+    mySprite3 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    mySprite3,
+    [img`
+        . . . . . . c c c . . . . . . . 
+        . . . . . . c 5 b c . . . . . . 
+        . . . . c c c 5 5 c c c . . . . 
+        . . c c c c 5 5 5 5 c b c c . . 
+        . c b b 5 b 5 5 5 5 b 5 b b c . 
+        . c b 5 5 b b 5 5 b b 5 5 b c . 
+        . . c 5 5 5 b b b b 5 5 5 f . . 
+        . . f f 5 5 5 5 5 5 5 5 f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . . f f f f f f f f f f . . . 
+        . . . e e f f f f f f f e . . . 
+        . . e b f b 5 b b 5 b c b e . . 
+        . . e e f 5 5 5 5 5 5 f e e . . 
+        . . . . c b 5 5 5 5 b c . . . . 
+        . . . . . f f f f f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c . . . . . . 
+        . . . . . . . c 5 c . . . . . . 
+        . . . . c c c 5 5 c c c . . . . 
+        . . c c b c 5 5 5 5 c c c c . . 
+        . c b b 5 b 5 5 5 5 b 5 b b c . 
+        . c b 5 5 b b 5 5 b b 5 5 b c . 
+        . . f 5 5 5 b b b b 5 5 5 c . . 
+        . . f f 5 5 5 5 5 5 5 5 f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . . f f f f f f f f f f . . . 
+        . . . e b e e f f f f b b e . . 
+        . . . e b b e b b 5 5 f e e . . 
+        . . . . c e e 5 5 5 5 5 f . . . 
+        . . . . . f f f f f f f . . . . 
+        `,img`
+        . . . . . . . c c c . . . . . . 
+        . . . . . . c b 5 c . . . . . . 
+        . . . . c c c 5 5 c c c . . . . 
+        . . c c b c 5 5 5 5 c c c c . . 
+        . c b b 5 b 5 5 5 5 b 5 b b c . 
+        . c b 5 5 b b 5 5 b b 5 5 b c . 
+        . . f 5 5 5 b b b b 5 5 5 c . . 
+        . . f f 5 5 5 5 5 5 5 5 f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . . f f f f f f f f f f . . . 
+        . . . e e f f f f f f e e . . . 
+        . . e b c b 5 b b 5 b f b e . . 
+        . . e e f 5 5 5 5 5 5 f e e . . 
+        . . . . c b 5 5 5 5 b c . . . . 
+        . . . . . f f f f f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . c c . . . . . . . . 
+        . . . . . . c 5 c . . . . . . . 
+        . . . . c c c 5 5 c c c . . . . 
+        . . c c c c 5 5 5 5 c b c c . . 
+        . c b b 5 b 5 5 5 5 b 5 b b c . 
+        . c b 5 5 b b 5 5 b b 5 5 b c . 
+        . . c 5 5 5 b b b b 5 5 5 f . . 
+        . . f f 5 5 5 5 5 5 5 5 f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . f f f f f f f f f f f f . . 
+        . . . f f f f f f f e e f . . . 
+        . . e b b f e e e e e b e . . . 
+        . . e e f 5 5 b b e b b e . . . 
+        . . . f 5 5 5 5 5 e e c . . . . 
+        . . . . f f f f f f f . . . . . 
+        `],
+    200,
+    true
+    )
+    mySprite3.setPosition(mySprite.x - randint(0, 12), 120)
+    mySprite3.setVelocity(0, -50)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy()
+    spawnEnemy()
+})
+let mySprite3: Sprite = null
 let boolAttackOverride = false
 let mySprite2: Sprite = null
+let mySprite: Sprite = null
 let boolWeaponOut = false
 let AButtonLastPressedTime = 0
 let arrayCurrentWeaponImages: Image[] = []
@@ -53,9 +164,11 @@ arrayCurrentWeaponImages = arrayWeapon1Images
 AButtonLastPressedTime = 0
 boolWeaponOut = false
 scene.setBackgroundColor(13)
-let mySprite = sprites.create(assets.image`player`, SpriteKind.Player)
-mySprite2 = sprites.create(arrayCurrentWeaponImages[0], SpriteKind.Player)
+mySprite = sprites.create(assets.image`player`, SpriteKind.Player)
+mySprite2 = sprites.create(arrayCurrentWeaponImages[0], SpriteKind.Weapon)
+scene.cameraFollowSprite(mySprite)
+spawnEnemy()
 game.onUpdate(function () {
-    mySprite2.x = mySprite.x
-    mySprite2.top = mySprite.bottom - 5
+    mySprite2.x = mySprite.x - 4
+    mySprite2.top = mySprite.bottom - 4
 })
